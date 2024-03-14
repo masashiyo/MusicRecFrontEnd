fd642dcf016a0320637dd1633e9adcf25c1f1433
Masashi Yoshida <ymasashi3@gmail.com>
2024-03-14 09:23:37 -0400

    refactored the top songs and artist page

--- src/pages/TopSongsAndArtists.jsx
+++ src/pages/TopSongsAndArtists.jsx
@@ -8,45 +8,38 @@ const TopTracksAndArtists = () => {
   const [mappedArtists, setMappedArtists] = useState([]);
   const [mappedTracks, setMappedTracks] = useState([]);
   const [currentButtonClicked, setCurrentButtonClicked] = useState('');
+  const [fetching, setFetching] = useState(false);
 
+  const fetchData = (endpoint, setterFunction, buttonClickedSetter) => {
+    setFetching(true);
+    setCurrentButtonClicked(buttonClickedSetter);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        limit: 10,
+        offset: 0,
+        timeRange: 'long_term'
+      })
+    };
+  
+    fetch(`http://localhost:8080/api/${endpoint}`, requestOptions)
+      .then((response) => response.json())
+      .then(data => {
+        setterFunction(data);
+        console.log(data);
+        setFetching(false);
+      });
+  };
+  
   const getTopArtists = () => {
-    setCurrentButtonClicked("Artists")
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        limit: 10,
-        offset: 0,
-        timeRange: 'long_term'
-      })
-    };
+    fetchData('user-top-artists', setUserTopArtists, 'Artists');
+  };
   
-    fetch("http://localhost:8080/api/user-top-artists", requestOptions)
-      .then((response) => response.json())
-      .then(data => {
-        setUserTopArtists(data)
-        console.log(data)
-      })
-  }
   const getTopTracks = () => {
-    setCurrentButtonClicked("Tracks")
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        limit: 10,
-        offset: 0,
-        timeRange: 'long_term'
-      })
-    };
+    fetchData('user-top-tracks', setUserTopTracks, 'Tracks');
+  };
   
-    fetch("http://localhost:8080/api/user-top-tracks", requestOptions)
-      .then((response) => response.json())
-      .then(data => {
-        setUserTopTracks(data)
-        console.log(data)
-      })
-  }
 
   useEffect(() => {
     if(userTopArtists.length > 0) {
@@ -71,24 +64,35 @@ const TopTracksAndArtists = () => {
         <h1 className="title">Get most listened to artists or tracks!</h1>
         <br></br>
         <div>
-          <label>Short Term </label>
-          <input type="radio" id="shortTerm" name="term" value="short_term"></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <label>Medium Term </label>
-          <input type="radio" id="mediumTerm" name="term" value="medium_term"></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <label>Long Term </label>
-          <input type="radio" id="longTerm" name="term" value="long_term"></input>
+
+          <label>Short Term&nbsp;&nbsp;
+            <input type="radio" id="shortTerm" name="term" value="short_term"></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </label>
+
+          <label>Medium Term&nbsp;&nbsp;
+            <input type="radio" id="mediumTerm" name="term" value="medium_term"></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </label>
+          
+          <label>Long Term&nbsp;&nbsp;
+            <input type="radio" id="longTerm" name="term" value="long_term"></input>
+          </label>
+            
         </div><br></br>
-        <label># of Artists/Tracks </label>
-        <div>
-          <input type="text" id="number"></input>
-        </div><br></br>
+        <label># of Artists/Tracks
+          <div>
+            <input type="text" id="number"></input>
+          </div><br></br>
+        </label>
         <button className="button" onClick={getTopArtists}>Get Artists</button>
         <button className="button" onClick={getTopTracks}>Get Tracks</button>
       </header>
       <div className="artist-section">
         <h2 className="artist-title">{currentButtonClicked}</h2>
         <div className="artist-list">
-          {currentButtonClicked === "Artists" ? mappedArtists : mappedTracks}
+        {fetching ? <h1>Loading...</h1> 
+            :
+            (currentButtonClicked === "Artists" ? mappedArtists : mappedTracks)
+          }
         </div>
       </div>
     </div>

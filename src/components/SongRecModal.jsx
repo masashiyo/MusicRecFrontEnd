b85b9570754d51843a7f6c22cb8f8c85c7a4f024
Masashi Yoshida <ymasashi3@gmail.com>
2024-04-25 21:02:49 -0400

    added closing of songrec modal when clicking on the outside

--- src/components/SongRecModal.jsx
+++ src/components/SongRecModal.jsx
@@ -14,8 +14,8 @@ export default function SongRecModal(props) {
     return (
         <>
           {props.modal &&
-            <div className="fixed inset-0 z-50 overflow-hidden flex justify-center items-center bg-black bg-opacity-50">
-              <div className="bg-white rounded-lg w-4/5 md:w-[70%] h-4/5 md:h-[80%] overflow-y-auto relative">
+            <div className="fixed inset-0 z-50 overflow-hidden flex justify-center items-center bg-black bg-opacity-50" onClick={() => props.toggleModal()}>
+              <div className="bg-white rounded-lg w-4/5 md:w-[70%] h-4/5 md:h-[80%] overflow-y-auto relative" onClick={(e) => e.stopPropagation()}>
                 <button className="absolute top-2 right-2 bg-green-500 hover:bg-red-500 text-white font-semibold py-2 px-6 rounded-full shadow-md transition duration-200" onClick={() => props.toggleModal()}>Close</button>
                 <h2 className="text-5xl mb-12 mt-8 text-center text-green-500 font-semibold">Recommended Songs</h2>
                 <div className="flex flex-col items-center w-[70%] mx-auto">

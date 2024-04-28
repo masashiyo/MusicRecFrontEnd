fdcaa58574dffaa7cad2d1dc7c4e88912903ada2
Masashi Yoshida <ymasashi3@gmail.com>
2024-04-27 22:29:37 -0400

    simplified the songRecModal

--- src/components/SongRecModal.jsx
+++ src/components/SongRecModal.jsx
@@ -2,15 +2,6 @@ import React, { useState, useEffect } from "react";
 import TrackCard from "./TrackCard";
 
 export default function SongRecModal(props) {
-    const [mappedTracks, setMappedTracks] = useState([]);
-
-    useEffect(() => {
-        const trackData = props.trackList.map((track) => {
-            return <TrackCard key={track.id} track={track} previewMusic={true}/>
-          });
-          setMappedTracks(trackData);
-    },[props.trackList])
-
     return (
         <>
           {props.modal &&
@@ -19,7 +10,9 @@ export default function SongRecModal(props) {
                 <button className="absolute top-2 right-2 bg-green-500 hover:bg-red-500 text-white font-semibold py-2 px-6 rounded-full shadow-md transition duration-200" onClick={() => props.toggleModal()}>Close</button>
                 <h2 className="text-5xl mb-12 mt-8 text-center text-green-500 font-semibold">Recommended Songs</h2>
                 <div className="flex flex-col items-center w-[70%] mx-auto">
-                  {mappedTracks.length > 0 && mappedTracks}
+                  {props.trackList.map((track) => {
+                      return <TrackCard key={track.id} track={track} previewMusic={true}/>
+                    })}
                 </div>
                 <p className="text-3xl text-center mt-8 mb-4 text-gray-700">{props.fetchingTracks && "Loading..."}</p>
                 {!props.fetchingTracks &&
